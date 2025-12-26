3cb4cc27dcc9c4ef546ffa40a48647f7ac8de2e2
Man Patel <man.mp07@gmail.com>
2025-12-26 11:37:50 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-const page = () => {
-  return (
-    <div>Welcome to NextJS 16!</div>
-  )
+import ExploreBtn from "@/components/ExploreBtn";
+
+const Page = async () => {
+
+    return (
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+        
+            <ExploreBtn />
+        </section>
+    )
 }
 
-export default page
+export default Page;
